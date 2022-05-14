690ccb0e0569b826a5b5729333f52ecc51c2bc5c
MangelDark <yacalos_@hotmail.com>
2022-05-13 23:27:25 -0400

    Esquema del customermapper terminado.

--- Domain.Layer/EntityMapper/CustomerMapper.cs
+++ Domain.Layer/EntityMapper/CustomerMapper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain.Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +9,23 @@ using System.Threading.Tasks;
 
 namespace Domain.Layer.EntityMapper
 {
-    public class CustomerMapper 
+    public class CustomerMapper : IEntityTypeConfiguration<Customer>
     {
 
-        public void Configure()
+        public void Configure(EntityTypeBuilder<Customer> builder)
         {
 
+            builder.HasKey(x => x.Id).HasName("pk_customerid");
+            builder.Property(x => x.Id).ValueGeneratedOnAdd().HasColumnName("Id").HasColumnType("int");
+
+            builder.Property(x => x.Name).HasColumnName("Name").HasColumnType("nvarchar(200)").IsRequired(true);
+            builder.Property(x => x.LastName).HasColumnName("LastName").HasColumnType("nvarchar(250)").IsRequired(true);
+            builder.Property(x => x.Email).HasColumnName("").HasColumnType("nvarchar(200)").IsRequired(true);
+
+
+            builder.Property(x => x.Created).HasColumnName("Created_date").HasColumnType("datetime");
+            builder.Property(x => x.Modified).HasColumnName("Modified_date").HasColumnType("datetime");
+            builder.Property(x => x.IsActive).HasColumnName("IsActive").HasColumnType("bool");
 
 
         }
