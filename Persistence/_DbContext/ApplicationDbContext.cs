19588668e0e2e53e10a42790735ac7c782c6357c
MangelDark <yacalos_@hotmail.com>
2022-05-13 23:49:33 -0400

    Configuracion de DbContext con su connectionString en el appsetting

---
+++ Persistence/_DbContext/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Layer.EntityMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Layer._DbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+
+
+            model.ApplyConfiguration(new CustomerMapper());
+            base.OnModelCreating(model);
+           
+        }
+    }
+}
