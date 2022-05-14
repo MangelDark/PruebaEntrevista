e61fd1d7dd400d5a887c6b709c4ffa30a4aab252
MangelDark <yacalos_@hotmail.com>
2022-05-13 23:16:23 -0400

    Creacion de la entidad y la configuracion de las regla para la validacion

---
+++ Domain.Layer/Models/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Layer.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}
